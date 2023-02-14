8fa86697d415d3100de9105f8bacb424211804af
mstrdav <colin.geindre@gmail.com>
2023-02-14 15:05:20 +0100

    basic db code

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || 'myapp',
+  ssl: process.env.DB_SSL === 'true',
+});
+
+module.exports = pool;
+
