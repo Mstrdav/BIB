99b945c5773efbc68f6bce630313db4bf8e64f5d
mstrdav <colin.geindre@gmail.com>
2023-02-21 15:13:27 +0100

    updated connection information (only env)

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'myapp',
-  ssl: process.env.DB_SSL === 'true',
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
 });
 
 module.exports = pool;
-
