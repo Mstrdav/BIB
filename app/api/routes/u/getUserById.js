8e39a487bd067bbcbc14358dc272cc6a87c9764a
mstrdav <colin.geindre@gmail.com>
2023-03-19 16:57:53 +0100

    added user model

--- app/api/routes/u/getUserById.js
+++ app/api/routes/u/getUserById.js
@@ -1,16 +1,18 @@
 const db = require("../../../../config/db");
+const User = require("../../../models/User");
 
 // Retrieve a specific user by ID from the database
 module.exports = (req, res) => {
   const id = req.params.id;
-  db.query("SELECT * FROM tbl_static_user WHERE id = $1", [id], (error, results) => {
+  db.query("SELECT * FROM tbl_static_user WHERE user_id = $1", [id], (error, results) => {
     if (error) {
       console.error(error);
       res.status(500).json({ error: "Internal server error" });
     } else if (results.rows.length === 0) {
       res.status(404).json({ error: "User not found" });
     } else {
-      res.status(200).json(results.rows[0]);
+      let user = new User(results.rows[0]);
+      res.status(200).json(user.long());
     }
   });
 };
