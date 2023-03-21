6d28b2797a147b310e69a8a607eda877d4994988
mstrdav <colin.geindre@gmail.com>
2023-03-21 18:29:47 +0100

    fixed error with object notation

--- app/api/routes/u/getAllUsers.js
+++ app/api/routes/u/getAllUsers.js
@@ -10,19 +10,18 @@ module.exports = (req, res) => {
       if (err) {
         res.status(500).send("Error retrieving users from database");
       } else {
-        console.log(results);
-
         // Create an array of users
         let users = [];
         // Loop through the results
         for (let i = 0; i < results.rowCount; i++) {
           // Create a new user object
-          let user = new User(
-            results.rows[i].user_id,
-            results.rows[i].user_mail,
-            results.rows[i].user_pwd,
-            results.rows[i].user_role_id
-          );
+          let user = new User({
+            "user_id": results.rows[i].user_id,
+            "user_name": results.rows[i].user_name,
+            "user_mail": results.rows[i].user_mail,
+            "user_pp_url": results.rows[i].user_pp_url,
+            "role_id": results.rows[i].role_id
+          });
           // Add the user to the array
           users.push(user);
         }
