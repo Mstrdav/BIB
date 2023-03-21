1a7afdfd70daa0022828ad4f89c8c22e79fe1ea3
mstrdav <colin.geindre@gmail.com>
2023-03-21 15:33:41 +0100

    changed email to comply with regex test

--- seeders/adminUser.sql
+++ seeders/adminUser.sql
@@ -1,6 +1,6 @@
 -- create admin user if there is none
 INSERT INTO tbl_static_user(user_name, user_mail, user_pwd, user_pp_url)
-SELECT 'admin', 'admin@localhost', 'password', 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y&d=identicon'
+SELECT 'admin', 'admin@example.com', 'password', 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y&d=identicon'
 WHERE NOT EXISTS (SELECT 1 FROM tbl_static_user WHERE user_name = 'admin');
 
 -- create admin role if there is none
